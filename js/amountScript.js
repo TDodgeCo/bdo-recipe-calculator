5debad5884aa6cd45754baf1258f4328c2e58bb8
Tim Dodge <timldodge@gmail.com>
2021-10-13 13:48:59 -0600

    fixed the #complete window redirect issue

--- js/amountScript.js
+++ js/amountScript.js
@@ -1,11 +1,10 @@
 document.getElementById('submit').addEventListener('click', function(event) {
-  event.preventDefault()
-  var theRecipe = document.getElementById('recipeSelect').value
-  var theIngredient = document.getElementById('ingredientSelect').value
-  var theAmount = document.getElementById('amountSelect').value
+    event.preventDefault()
+    var theRecipe = document.getElementById('recipeSelect').value
+    var theIngredient = document.getElementById('ingredientSelect').value
+    var theAmount = document.getElementById('amountSelect').value
 
-  document.getElementById('ingList').classList.remove('hide-el')
-  document.getElementById('instructions').classList.toggle('hide-el')
-  window.location.replace('/#completed')
-  calcIngredients(theRecipe, theIngredient, theAmount)
+    document.getElementById('ingList').classList.remove('hide-el')
+    document.getElementById('instructions').classList.toggle('hide-el')
+    calcIngredients(theRecipe, theIngredient, theAmount)
 })
